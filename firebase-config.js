16faab39157429b1961b5dd060f6e095ab0dad21
unlimited-earning <munna6825ar@gmail.com>
2026-07-13 22:06:40 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFKWIm1kUaP47rsHtEEP05G66-SSRsvwA",
+  authDomain: "gamil-sever.firebaseapp.com",
+  databaseURL: "https://gamil-sever-default-rtdb.firebaseio.com",
+  projectId: "gamil-sever",
+  storageBucket: "gamil-sever.firebasestorage.app",
+  messagingSenderId: "895391304528",
+  appId: "1:895391304528:web:4b86c59ce3163607beba55"
+};
